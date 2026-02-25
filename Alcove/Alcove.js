1984d2912338aa6e9545dc87695ca163c84ecae0
InjectX <injectxteam@gmail.com>
2026-02-25 13:32:38 +0800

    fixed for alcove 1.6.5

--- Alcove/Alcove.js
+++ Alcove/Alcove.js
@@ -3,6 +3,7 @@ let url = $request.url;
 const license$ = {
     key: "88888888-8888-8888-8888-888888888888",
     active: true,
+    beta: false,
     name: "InjectX-Team",
     email: "injectxteam@gmail.com",
     limit: 999,
@@ -22,8 +23,8 @@ const trial$ = {
 
 const endpoints = [
     { pattern: /^https:\/\/api\.tryalcove\.com\/trial\/([A-F0-9-]+)$/i, response: trial$, signature: "12476fa503ed65e9ce88fc47c6d0b990380aa43787d2153a794f2a2b90558882"}, // Trial 接口
-    { pattern: /^https:\/\/api\.tryalcove\.com\/license\/validate$/i, response: license$, signature: "112f368a054b22dd30185cbbfad5aa70a73e26f52efca645bc65462575b9f362"}, // Validate 接口
-    { pattern: /^https:\/\/api\.tryalcove\.com\/license\/activate$/i, response: license$, signature: "112f368a054b22dd30185cbbfad5aa70a73e26f52efca645bc65462575b9f362"} // Validate 接口
+    { pattern: /^https:\/\/api\.tryalcove\.com\/license\/validate$/i, response: license$, signature: "dd92f03c2e9e400e79a3eded5facd00982b76e9b0de7c86a9aa2ab1aa7d2a89b"}, // Validate 接口
+    { pattern: /^https:\/\/api\.tryalcove\.com\/license\/activate$/i, response: license$, signature: "dd92f03c2e9e400e79a3eded5facd00982b76e9b0de7c86a9aa2ab1aa7d2a89b"} // Validate 接口
 ];
 
 let alcoveHandler = () => {
